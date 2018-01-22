5e9dd0896db417a22ecf3568e385f5b9c4edd430
Kurt Tamulonis <kurttamulonis@gmail.com>
2018-01-22 10:47:50 -0500

    added , dependent: :destroy to users association to shots

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   attr_accessor :oauth_callback
   attr_accessor :current_password
   has_many :spins, dependent: :destroy
-  has_many :shots, through: :spin
+  has_many :shots, through: :spin, dependent: :destroy
 
     
   validates_presence_of   :email, if: :email_required?
