bc7c5f3a5776cc8bd06ef9dc7e80a580c8c2b912
Kurt Tamulonis <kurttamulonis@gmail.com>
2018-01-22 11:04:44 -0500

    removed all depended destroys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :oauth_callback
   attr_accessor :current_password
-  has_many :spins, dependent: :destroy
-  has_many :shots, through: :spin, :dependent => :delete_all
+  has_many :spins
+  has_many :shots
 
     
   validates_presence_of   :email, if: :email_required?
