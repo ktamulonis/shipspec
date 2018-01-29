363b2ce36626d1e05a5bbff2fe787f078e932269
Kurt Tamulonis <kurttamulonis@gmail.com>
2018-01-29 13:46:05 -0500

    some show styling

--- app/models/spin.rb
+++ app/models/spin.rb
@@ -1,6 +1,6 @@
 class Spin < ApplicationRecord
   belongs_to :user
   has_many :shots, :dependent => :delete_all
-  has_attached_file :shot1, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :shot1, styles: { large: "1200x800>", medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :shot1, content_type: /\Aimage\/.*\z/
 end
