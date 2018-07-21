f61d46ff145a300ac9deea455589ffc71aba5645
Kurt Tamulonis <kurttamulonis@gmail.com>
2018-07-21 17:12:19 -0400

    added :dependent => :destroy to user model on spins relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :oauth_callback
   attr_accessor :current_password
-  has_many :spins, :dependent => :delete_all
+  has_many :spins, :dependent => :destroy
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
