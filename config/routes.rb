3a50b130e473bff4e423157f1360615144801dc9
Kurt Tamulonis <kurttamulonis@gmail.com>
2018-07-22 14:52:33 -0400

    gsubed all http for https

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/setup/open' => 'setup#open'
   post '/setup/generate' => 'setup#generate'
   get '/setup' => 'setup#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shot_setups
   resources :accounts
   resources :spins do
